8d241ea8e4b54e23d93843ecb1f2b9d1f0be037d
lichengfeng497 <1309837013@qq.com>
2026-08-26 14:09:20 +0800

    [ssbuffer](fix) merge compute blocks in computeBlockOptPass

--- third_party/ascend/lib/DynamicCVPipeline/ComputeBlockOptPass.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/ComputeBlockOptPass.cpp
@@ -77,6 +77,7 @@ void ComputeBlockOptPass::runOnOperation() {
   pm.addPass(createMergeSmallBlockPass());
   pm.addPass(createReorderOpsByBlockIdPass());
 
+  pm.addPass(createMergeComputeBlockPass());
   pm.addPass(createMergeCubeBlockPass());
   pm.addPass(createReorderOpsByBlockIdPass());
 
@@ -115,6 +116,7 @@ void registerComputeBlockOptPasses() {
   registerPass(createPosMaskPatternPass);
   registerPass(createMergeSmallBlockPass);
   registerPass(createSplitIfByBlockIdPass);
+  registerPass(createMergeComputeBlockPass);
   registerPass(createMergeCubeBlockPass);
   registerPass(createRelocateMemrefDeclPass);
 }
